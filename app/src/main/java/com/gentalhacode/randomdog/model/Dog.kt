44e44ce0b68cfa5d97ec5c03cb1a41dcb055d79a
thgMatajs <thiago.pacheco@truckpad.com.br>
2020-03-24 18:46:54 -0300

    config dependencies

---
+++ app/src/main/java/com/gentalhacode/randomdog/model/Dog.kt
@@ -0,0 +1,10 @@
+package com.gentalhacode.randomdog.model
+
+/**
+ * .:.:.:. Created by @thgMatajs on 24/03/20 .:.:.:.
+ */
+interface Dog {
+    val photoUrl: String
+}
+
+//https://random.dog/woof.json
